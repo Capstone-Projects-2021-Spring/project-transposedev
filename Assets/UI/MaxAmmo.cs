2bb6d9e3eeca680a743200d90b2ac9e18fc4edb1
Chin-Hung Lee <tuh22049@temple.edu>
2021-02-19 05:32:01 +0800

    added UI scene and ammo icon *testing*

---
+++ Assets/UI/MaxAmmo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaxAmmo : MonoBehaviour
+{
+    public static float MaxAmmoAmount = 10;
+    public Transform MaxAmmoText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MaxAmmoText.GetComponent<Text>().text = MaxAmmoAmount.ToString();
+    }
+}
