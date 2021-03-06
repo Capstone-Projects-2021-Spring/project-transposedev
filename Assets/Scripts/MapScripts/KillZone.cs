1b80bdb5a8306d05d734765dded46eae4a747f07
Erika Garcia <tug00581@temple.edu>
2021-03-06 03:12:47 -0500

    Added simple map, kill zone, and spawn

---
+++ Assets/Scripts/MapScripts/KillZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour {
+
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform spawnPoint;
+
+    private void OnTriggerEnter(Collider other) {
+        //Destroy(other.gameObject);
+        player.transform.position = spawnPoint.transform.position;
+    }
+
+}
