f0873c031ea1458b93398da364ae4756db0be3e1
justabird3 <59928547+justabird3@users.noreply.github.com>
2021-03-12 22:27:56 -0500

    Basic weapon implemented with target that takes damage

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour, IDamageable
+{
+	const float maxHealth = 100f;
+	float currentHealth = maxHealth;
+
+	void Update()
+	{
+		if (currentHealth <= 0)
+		{
+			Die();
+		}
+	}
+
+	public void TakeDamage(float damage)
+	{
+		currentHealth -= damage;
+		Debug.Log("Damaged! Health at: " + currentHealth);
+	}
+
+	void Die()
+	{
+		
+	}
+}
