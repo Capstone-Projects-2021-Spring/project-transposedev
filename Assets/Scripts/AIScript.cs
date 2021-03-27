926ef4b6f8cdb11d423e3226af393b368e2bf9d2
tug88673 <59925684+tug88673@users.noreply.github.com>
2021-03-26 21:51:19 -0400

    Finished AI Script template
    Values will be changed to correlate with the current setup of the project

--- Assets/Scripts/AIScript.cs
+++ Assets/Scripts/AIScript.cs
@@ -6,6 +6,8 @@ public class AIScript : MonoBehavior{
   public Transform player;
   public LayerMask GroundSensor;
   public LayerMask PlayerSensor;
+  public GameObject projectile;
+  public float health;
   
   public Vector3 walkpoint;
   bool walkpointSet;
@@ -39,11 +41,64 @@ public class AIScript : MonoBehavior{
     }   
  
  private void AttackMode(){
+      agent.SetDestination(transform.position);
+      transform.LookAt(player);
+      if(alreadyAttacked != true){
+         Rigidbody R = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+         R.addForce(transform.forward*32f, ForceMode.Impulse);
+         R.addForce(transform.up*8f, ForceMode.Impulse);
+         alreadyAttacked = true;
+         Invoke(nameOf(ResetAttack), attackCooldown);
+      }
     }
  
  private void PatrolMode(){
+      if (walkpointSet != true){
+         SearchWalkpoint();
+      }
+      else
+         agent.SetDestination(walkpoint);
+   
+      Vector3 distToWalkpoint = transform.position - walkpoint;
+      if(distToWalkpoint.magnitude < 1f){
+          walkpointSet = false;
+      }
     }
    
  private void ChaseMode(){
+      agent.SetDestination(player.position);
     }  
+ 
+ private void ResetAttack(){
+      alreadyAttacked = false;
+    } 
+  
+ private void SearchWalkpoint(){
+      float randomX = Random.Range(-walkpointRange, walkpointRange);
+      float randomZ = Random.Range(-walkpointRange, walkpointRange);
+      walkpoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+      if (Physics.Raycast(walkpoint, -transform.up, 2f, GroundSensor)){ //checks if walkpoint is on map
+        walkpointSet = true;
+      }
+    }
+  
+  public void TakeDamage(int damage){
+      health = health - damage;
+      if (health <= 0){
+        Invoke(nameOf(killEnemy), 0.5f);
+      }
+   }
+  
+  private void killEnemy(){
+      Destroy(gameObject);
+    }
+  
+  //uncomment to make sightRange & attackRange visible in-game
+  //private void makeSightRangesVisible() {
+  //Gizmos.color = Color.red;
+  //Gizmos.DrawWireSphere(transform.position, attackRange);
+  //Gizmos.color = Color.yellow;
+  //Gizmos.DrawWireSphere(transform.position, sightRange);
+  //}
+  
 }
