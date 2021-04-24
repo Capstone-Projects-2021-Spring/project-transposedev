7f86cbbd25bce4b41f84d04a8945339ad6c74d84
Alexander Rodriguez <59924920+Alexander-K-R@users.noreply.github.com>
2021-04-24 09:57:54 -0400

    Added the gravity power to PowerSounds for easier integration

---
+++ Assets/Scripts/Player Scripts/Classes/Gravitator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravitator : PlayerClass
+{
+    private Rigidbody rb;
+    [SerializeField] private AudioClip gravityClip;
+    private AudioSource gravitySource;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Start()
+    {
+        gravitySource = gameObject.AddComponent<AudioSource>() as AudioSource;
+        gravitySource.playOnAwake = false;
+        gravitySource.clip = gravityClip;
+    }
+
+    public override void UseAbility()
+    {
+        gravitySource.Play();
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            gravitySource.Play();
+            rb.useGravity = false;
+        }
+        else if (Input.GetButtonUp("Fire3"))
+        {
+            gravitySource.Play();
+            rb.useGravity = true;
+        }
+    }
+
+    public override void UseAltAbility()
+    {
+        throw new System.NotImplementedException();
+    }
+}
